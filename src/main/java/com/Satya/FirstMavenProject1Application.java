839a2f5f03fbe72d740ef8dcb690b73f496fae0a
satyabrata dhir <55347942+satyabratadhir5697@users.noreply.github.com>
2024-08-12 15:15:25 +0530

    Update FirstMavenProject1Application.java

--- src/main/java/com/Satya/FirstMavenProject1Application.java
+++ src/main/java/com/Satya/FirstMavenProject1Application.java
@@ -1,5 +1,5 @@
 package com.Satya;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
